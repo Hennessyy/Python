5f1d40c77062d81ed0eb7ff5103a6fb149796eda
Hennessyy <leon.hennessyy@gmail.com>
2022-02-14 18:14:06 +0000

    improved

--- Codewars/sum_strings.py
+++ Codewars/sum_strings.py
@@ -1,9 +1,2 @@
 def sum_str(a, b):
-    if a and b:
-        return str(int(a) + int(b))
-    elif not a and b:
-        return b
-    elif a and not b:
-        return a
-    else:
-        return '0'
+    return str(int(a or 0) + int(b or 0))
